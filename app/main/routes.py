744677640ea35f163532839c269332d70c0117c5
Alexander Danson <alex.danson2@gmail.com>
2020-06-23 09:31:00 +0200

    adding music page

--- app/main/routes.py
+++ app/main/routes.py
@@ -16,6 +16,18 @@ def index():
         flash("Thanks for your email, I'll get back to you as soon as I can!")
     return render_template('index.html', form=form)
 
+@bp.route('/music', methods=['GET', 'POST'])
+def music():
+    form = ContactForm()
+    if form.validate_on_submit():
+        from_name = form.name.data
+        sender = form.email.data
+        message_body = form.message.data
+        phone = form.phone.data
+        contact_me_email(from_name=from_name, sender=sender, message_body=message_body, phone=phone)
+        flash("Thanks for your email, I'll get back to you as soon as I can!")
+    return render_template('music.html', form=form)
+
 @bp.route('/test')
 def test():
     return render_template('test.html')
